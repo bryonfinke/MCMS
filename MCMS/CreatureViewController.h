aac515468022139ff164196677450ca75b6a9b3b
Brittany Kimbrough <bkimbrough87@gmail.com>
2015-05-19 14:40:46 -0500

    add the creature view

---
+++ MCMS/CreatureViewController.h
@@ -0,0 +1,15 @@
+//
+//  CreatureViewController.h
+//  MCMS
+//
+//  Created by Brittany Kimbrough on 5/19/15.
+//  Copyright (c) 2015 Bryon Finke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MagicalCreature.h"
+
+
+@interface CreatureViewController : UIViewController
+
+@end
---
+++ MCMS/CreatureViewController.m
@@ -0,0 +1,39 @@
+//
+//  CreatureViewController.m
+//  MCMS
+//
+//  Created by Brittany Kimbrough on 5/19/15.
+//  Copyright (c) 2015 Bryon Finke. All rights reserved.
+//
+
+#import "CreatureViewController.h"
+#import "MagicalCreature.h"
+
+@interface CreatureViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *creatureName;
+
+@end
+
+@implementation CreatureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
